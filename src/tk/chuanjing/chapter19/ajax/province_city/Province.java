92dea3e6a5f718064e096a1770c1626b9e0ed8a8
gcj <longgongsanri@163.com>
2017-12-15 21:25:06 +0800

    ajax，省市联动

---
+++ src/tk/chuanjing/chapter19/ajax/province_city/Province.java
@@ -0,0 +1,19 @@
+package tk.chuanjing.chapter19.ajax.province_city;
+
+public class Province {
+	private int pid;
+	private String pname;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	
+}
